a632b8d2de5dd07c472d5ee980b7d6d958ea0510
Valera <example@gmail.com>
2023-07-31 13:46:12 +0300

    first commit

---
+++ sample.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log('arr' + ' - jbcect');
+console.log(5 + "5");
+
+
+let incr = 10;
+let decr = 10;
+
+//++incr;
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(2 + 2 * 2 != '6');
+
+const isChecked = false;
+const isClose = false;
+
+console.log(isChecked || !isClose);

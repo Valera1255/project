a479fac80dd2c26930a4656bda33c46c61818398
Valera <example@gmail.com>
2023-09-24 21:21:12 +0300

    Задачи на собеседование
    простые таски

---
+++ tasks.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//let x = 5; alert( ++x ); 6
+
+//let num = []; let result = num + false - null + true; console.log(typeof(result)); = NaN
+
+//let y = 1; let x = y = 2; alert(x); = 2
+
+//let arr = []; let result = arr + 1 + 2; console.log(result);
+
+//alert("1"[0]);
+
+//console.log(2 && 1 && null && 0 && undefined); null
+
+//let a = 1; let b = 2; console(a && b);
+// && запинаєтся на лжи
+// || запинаєтся на правде
+
+//console.log(!!( 1 &&  2) === (1 && 2)); - не равны
+//!!( 1 &&  2) - boolean
+
+
+//alert( null || 2 && 3 || 4 );
+// || запинаєтся на правде
+
+//let a = [1, 2, 3]; let b = [1, 2, 3]; console.log(a == b);
+//два разных хранилища информации, поэтому они не будут равны
+
+//alert( +"Infity" ); //NaN
+
+//let word1 = "Ёжик"; let word2 = "Яблоко"; console.log(word1 > word2);
+
+//console.log(0 || "" || 2 || undefined || true || false); = 2

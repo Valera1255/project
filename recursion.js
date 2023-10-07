eb241a24e9e949744dab756f599b2e2ba9237f27
Valera <example@gmail.com>
2023-10-07 21:34:23 +0300

    Recursion

---
+++ recursion.js
@@ -0,0 +1,131 @@
+"use strict";
+
+// функция возведения в степень
+// рекурсия это функция которая сама себя запеускает для вычисления каких-то действи
+
+
+// первий вариант
+/*function pow(x, n) {
+    let result = 1;
+
+    for (let i = 0; i < n; i++ );{
+        result *=x;
+        console.log(result);
+    }
+    return result;
+}*/
+
+
+
+// рекурсия сводит к базе к начальному реузультату даже если большие числа
+// pow(2, 1); // 2 n = 1
+
+//шаг рекурсии
+// база рекурсии
+//глубина рекурсии
+
+
+// втрой вариант
+/*function pow(x, n) {
+ if(n === 1) {
+    return x;
+ } else{
+    return x * pow(x, n - 1);
+ }
+}
+
+pow(2, 1); // 2 
+pow(2, 2); // 4
+*/
+
+
+//Примепр по итератору
+let students = {
+    js: [{
+        name: 'John',
+        progress: 100
+    }, {
+        name: 'Ivan',
+        progress: 60
+    }],
+
+    html: {
+        basic: [{
+            name: 'Peter',
+            progress: 20
+        }, {
+            name: 'Ann',
+            progress: 18
+        }],
+    
+        pro: [{
+        name: 'Sam',
+        progress: 10
+        }],
+        
+        semi: {
+            students: [{
+                name: 'Test',
+                progress: 100
+            }]
+        }
+    }
+};
+
+// метод Object.values() - масив значений перечисляемых свойств
+// метод Array.isArray() - возвращает true, если объэкт это масив и false, если не массив
+
+function getTotalProgressByIteration (data) {
+    let total = 0;
+    let students = 0;
+    
+    for (let course of Object.values(data)){
+        if (Array.isArray(course)) {
+            students += course.length;
+
+            for (let i = 0; i < course.length; i++) {
+                total += course[i].progress;
+            }
+        } else {
+            for (let subCourse of Object.values(course)) {
+                students += subCourse.length;
+
+                for (let i = 0; i < subCourse.length; i++) {
+                    total += subCourse[i].progress;
+                }
+            }
+        }
+    }
+    return total / students; 
+}
+
+
+//console.log(getTotalProgressByIteration(students));
+
+//Примепр с использованием рекурсии
+// база проверка на масив
+
+function getTotalProgressByRecursion(data) {
+    if (Array.isArray(data)) {
+        let total = 0;
+        
+        for (let i = 0; i < data.length; i++) {
+            total += data[i].progress;
+        }
+
+        return [total, data.length];
+    } else {
+        let total = [0, 0];
+
+        for(let subData of Object.values(data)){
+            const subDataArr = getTotalProgressByRecursion(subData);
+            total[0] += subDataArr[0];
+            total[1] += subDataArr[1];  
+        }
+        return total;
+    }
+}
+
+const result = getTotalProgressByRecursion(students);
+
+console.log(result[0]/result[1]);
